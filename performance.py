06ed936f16e70213d99023b57e6b78abf96b2863
Sai Shibu N B <saishibu38380@gmail.com>
2019-07-02 16:59:23 +0530

    Update performance.py

--- performance.py
+++ performance.py
@@ -5,8 +5,12 @@ conn = pymysql.connect(database="BC",user="admin",password="admin",host="localho
 cur=conn.cursor()
 
 print("Temperature")
-temp=subprocess.check_output("vcgencmd measure_temp | awk 'NR==1 {print $1}'", shell=True)
-temp=float(temp[5:9])
+try:
+  temp=subprocess.check_output("vcgencmd measure_temp | awk 'NR==1 {print $1}'", shell=True)
+  temp=float(temp[5:9])
+except:
+  temp=subprocess.check_output(cat /sys/class/thermal/thermal_zone0/temp), shell=True)
+  
 print(temp)
 
 print("Available-RAM")
