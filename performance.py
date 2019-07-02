8cdd730d6fc9f4fd4e4bd4749fdcefd1d481f053
Sai Shibu N B <saishibu38380@gmail.com>
2019-07-02 16:56:36 +0530

    Update performance.py

--- performance.py
+++ performance.py
@@ -4,23 +4,22 @@ import subprocess,datetime,pymysql
 conn = pymysql.connect(database="BC",user="admin",password="admin",host="localhost")
 cur=conn.cursor()
 
-print("Temperature \n")
+print("Temperature")
 temp=subprocess.check_output("vcgencmd measure_temp | awk 'NR==1 {print $1}'", shell=True)
-temp=temp[5:9]
+temp=float(temp[5:9])
 print(temp)
 
 print("Available-RAM")
-RAM=subprocess.check_output("free | awk 'NR==2 {print $7}'",shell=True)
+RAM=int(subprocess.check_output("free | awk 'NR==2 {print $7}'",shell=True))
 print(RAM)
 
 print("CPU usage in %")
-print("\n")
-CPU=subprocess.check_output("top -n1 | awk '/Cpu\(s\):/ {print $2}'",shell=True)
+CPU=float(subprocess.check_output("top -n1 | awk '/Cpu\(s\):/ {print $2}'",shell=True))
 print(CPU)
 
 print("Disk Usage in %")
 disk=subprocess.check_output("df -h --total | awk 'NR==11 {print $5}'",shell=True)
-#disk=disk[:2]
+disk=float(disk[:2])
 print(disk)
 
 data={'temp':temp,'CPU':CPU,'RAM':RAM,'disk':disk}
