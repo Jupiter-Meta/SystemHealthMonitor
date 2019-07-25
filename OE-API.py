97fff6ee1740327729b9f1303ae5b81b7ddf47bd
Sai Shibu N B <saishibu38380@gmail.com>
2019-07-25 11:05:01 +0530

    Update OE-API.py

--- OE-API.py
+++ OE-API.py
@@ -9,7 +9,7 @@ urls=("/favicon.ico","dummy")
 
 @app.route('/')
 def API1():
-	data = {'EnergyAvailability':random.random(0,10),'CostPerUnit':random.random(1,6)}
+	data = {'EnergyAvailability':random.uniform(0,10),'CostPerUnit':random.uniform(1,6)}
 	return jsonify(data)
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=2020,debug=1)
