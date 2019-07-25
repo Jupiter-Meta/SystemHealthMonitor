176b366aac7fb4d95969a9d0bf2181271849d398
Sai Shibu N B <saishibu38380@gmail.com>
2019-07-25 11:03:54 +0530

    Update OE-API.py

--- OE-API.py
+++ OE-API.py
@@ -2,14 +2,14 @@
 import datetime,sys,os,io
 from flask import Flask, jsonify,request
 from flaskext.mysql import MySQL
-
+import random
 app = Flask(__name__)
 mysql = MySQL()
 urls=("/favicon.ico","dummy")
 
 @app.route('/')
 def API1():
-	data = {'EnergyAvailability':rand(0,10),'CostPerUnit':rand(1,6)}
+	data = {'EnergyAvailability':random.random(0,10),'CostPerUnit':random.random(1,6)}
 	return jsonify(data)
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=2020,debug=1)
